fe097cd33032ab04990d4c0331b93384f475a4eb
CodeforLiberty <sswell@live.com>
2017-01-23 14:38:39 -0500

    Setup complete form elements rendered

---
+++ client/js/Components/EnglishInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function EnglishInput () {
+
+	return (
+		<div>
+		<input type='text' placeholder='Enter word to translate'/>
+		</div>
+
+		);
+};
--- client/js/index.js
+++ client/js/index.js
@@ -1,3 +1,16 @@
 import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Test from './components/button';
+import EnglishInput from './components/EnglishInput';
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+ReactDOM.render(<EnglishInput/>,
+	document.getElementById('app')
+
+	);	
+
+  });
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
